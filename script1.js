c90ece49b71b707699b478be4f44de5b275b7320
K <kitkatapioca@gmail.com>
2025-05-26 11:21:24 -0400

    Initial commit

---
+++ script1.js
@@ -0,0 +1,17 @@
+//validar nombre, debe contener solo letras y no ser vacio
+//validar correo: Debe tener solo un @ y terminar en .cl
+
+let personas = []
+
+function validar(){
+    let vNombre = document.getElementById("nombre")
+    let eNombre = vNombre.value
+    let vApellido = document.getElementById("apellido")
+    let eApellido = vApellido.value
+    let eErrorNombre = document.getElementById("errorNombre")
+    let eErrorApellido = document.getElementById("errorApellido")
+
+}
+
+function vTexto(valor,elemento, error){
+}
